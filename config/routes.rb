dd5c476510d8d2aa9c7c716a77f6a6612a2fbc44
FukushimaTakeshi <whippet.818@gmail.com>
2017-11-20 00:30:00 +0900

    add seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'inquiry/new', to: 'inquiry#new'
-  # get 'entry/new', to: 'entry#new', as: 'entry'
   post 'inquiry/confirm', to: 'inquiry#confirm'
   post 'inquiry/finish', to: 'inquiry#finish'
-
 end
